1befe05af211a9e3879bdec66607de486742f042
Jeremy Douglass <jeremydouglass@gmail.com>
2017-09-12 10:38:01 -0700

    Assets: script to remove unicode from file lines
    specifically filename caches image_list.tsv and signatures_list.tsv

---
+++ _data/_unidecode_data.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+# using homebrew requirements:
+
+# pip2 install unidecode
+import unidecode
+import codecs
+import os
+
+#------------------------------------------------
+def unidecode_file_contents(filename):
+    """
+    """
+    orig_data=""
+    unidecode_data=""
+
+    with codecs.open(filename, encoding='utf-8', mode='r') as f:
+        orig_data = f.read()
+        unidecode_data = unidecode.unidecode(orig_data)
+
+    with codecs.open(filename, encoding='utf-8', mode='w+') as f:
+        f.write(unidecode_data)
+
+#------------------------------------------------
+if __name__ == "__main__":
+    # input is of type 'unicode'
+    unidecode_file_contents('image_list.tsv')
+    unidecode_file_contents('signatures_list.tsv')
+
+# https://stackoverflow.com/a/2633310/7207622
